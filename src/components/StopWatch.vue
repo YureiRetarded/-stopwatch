d6cb178abe60bdc5dde9e52fa0aa0c2dd9bfd302
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-31 18:28:50 +0300

    logic is working

---
+++ src/components/StopWatch.vue
@@ -0,0 +1,53 @@
+<template>
+    <span>{{h}}:{{m}}:{{s}}:{{ms}}</span>
+    <div>
+        <button @click="StopWatch('start')">Start</button>
+        <button @click="StopWatch('stop')">Stop</button>
+        <button @click="StopWatch('reset')">Reset</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            ms:0,
+            s:0,
+            m:0,
+            h:0,
+            interval:''
+        }
+    },
+    methods: {
+        StopWatch(action){
+            try{
+                let startStopWatch = () =>{
+                this.ms++
+            }
+            if(action=='start'){
+                clearInterval(this.interval);
+                this.interval = setInterval(startStopWatch, 10);
+
+            }
+            else if(action=='stop'){
+                clearInterval(this.interval);
+            }
+            else if(action=='reset'){
+                clearInterval(this.interval);
+                this.ms=0
+                this.s=0
+                this.m=0
+                this.h=0
+            }
+            }
+            catch{
+
+            }
+            
+        }
+    }
+}
+
+</script>
+<style scoped>
+    
+</style>
--- src/pages/mainpage.vue
+++ src/pages/mainpage.vue
@@ -2,10 +2,20 @@
     <div>
         Здесь будет приложение
     </div>
+    <stop-watch></stop-watch>
+
 </template>
 <script>
+import StopWatch from '@/components/StopWatch'
 export default {
-    
+    components:{
+        StopWatch
+    },
+    data(){
+        return{
+
+        }
+    }
 }
 </script>
 <style lang="">
