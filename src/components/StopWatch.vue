2edbe373618ef67ea5980c7147bc3a3984618c7c
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-05-31 18:41:02 +0300

    Ok. its working

--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -14,6 +14,7 @@ export default {
             s:0,
             m:0,
             h:0,
+            d:0,
             interval:''
         }
     },
@@ -22,6 +23,41 @@ export default {
             try{
                 let startStopWatch = () =>{
                 this.ms++
+                
+                //ms
+                if(this.ms<=9){
+                    this.ms='0'+this.ms
+                }
+                if(this.ms>99){
+                    this.s++
+                    this.ms=0
+                }
+
+                //s
+                if(this.s>60){
+                    this.m++
+                    this.s=0
+                    this.ms=0
+                }
+
+                //m
+                if(this.m>60){
+                    this.h++
+                    this.m=0
+                    this.s=0
+                    this.ms=0
+                }
+
+                //h
+                if(this.h>24){
+                    this.d++
+                    this.h=0
+                    this.m=0
+                    this.s=0
+                    this.ms=0
+                     //(I don't know, who will use stopwatch for days and why)
+
+                }
             }
             if(action=='start'){
                 clearInterval(this.interval);
