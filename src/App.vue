6c7878ac52c6c8f56ea96b8ab9bd5451a9726885
YureiRetarded <cy6m9qc5r7trjhdv@gmail.com>
2022-06-02 15:19:03 +0300

    Scss is installed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
+  <navbar/>
   <router-view/>
 </template>
+<script>
+import Navbar from "@/components/Navbar"
+export default{
+  components:{
+    Navbar
+  },
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
 
 <style>
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="navbar fixed-top">
+        Много текста
+    </nav>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss" scoped >
+
+</style>
